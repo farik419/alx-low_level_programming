8e63970792364ad55a9c32c78d5c4b01f8cb617c
faiz <faizfarouk8@gmail.com>
2023-03-02 09:44:29 -0800

    103-infinite_add.c

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -1,105 +1,71 @@
- #include "main.h"                                                                                                                       
-                                                                                                                                        
-char *add_strings(char *n1, char *n2, char *r, int r_index);                                                                            
-char *infinite_add(char *n1, char *n2, char *r, int size_r);                                                                            
-                                                                                                                                        
-/**                                                                                                                                     
- * add_strings - Adds the numbers stored in two strings.                                                                                
- * @n1: The string containing the first number to be added.                                                                             
- * @n2: The string containing the second number to be added.                                                                            
- * @r: The buffer to store the result.                                                                                                  
- * @r_index: The current index of the buffer.                                                                                           
- *                                                                                                                                      
- * Return: If r can store the sum - a pointer to the result.                                                                            
- *         If r cannot store the sum - 0.                                                                                               
- */                                                                                                                                     
-char *add_strings(char *n1, char *n2, char *r, int r_index)                                                                             
-{                                                                                                                                       
-        int num, tens = 0;                                                                                                                                
-        
-        for (; *n1 && *n2; n1--, n2--, r_index--)                                                                                       
-        {                                                                                                                               
-                num = (*n1 - '0') + (*n2 - '0');                                                                                        
-                num += tens;                                                                                                            
-                *(r + r_index) = (num % 10) + '0';                                                                                      
-                tens = num / 10;                                                                                                        
-        }                                                                                                                               
-                                                                                                                                        
-        for (; *n1; n1--, r_index--)                                                                                                    
-        {                                                                                                                               
-                num = (*n1 - '0') + tens;                                                                                               
-                *(r + r_index) = (num % 10) + '0';                                                                                      
-                tens = num / 10;                                                                                                        
-        }                                                                                                                               
-                                                                                                                                        
-        for (; *n2; n2--, r_index--)                                                                                                    
-        {                                                                                                                               
-                num = (*n2 - '0') + tens;                                                                                               
-                *(r + r_index) = (num % 10) + '0';                                                                                      
-                tens = num / 10;                                                                                                        
-        }                                                                                                                               
-                                                                                                                                        
-        if (tens && r_index >= 0)                                                                                                       
-        {                                                                                                                               
-                *(r + r_index) = (tens % 10) + '0';                                                                                     
-                return (r + r_index);                                                                                                   
-        }                                                                                                                               
-                                                                                                                                        
-        else if (tens && r_index < 0)                                                                                                   
-                return (0);                                                                                                             
-                                                                                                                                        
-        return (r + r_index + 1);                                                                                                       
+#include "main.h"
+/**
+ * rev_string - reverse array
+ * @n: integer params
+ * Return: 0
+ */
+void rev_string(char *n)
+{
+int i = 0;
+int j = 0;
+char temp;
+while (*(n + i) != '\0')
+{
+i++;
 }
-
-#include "main.h"                                                                                                                       
-#include <stdio.h>                                                                                                                      
-                                                                                                                                        
-/**                                                                                                                                     
- * print_buffer - Prints a buffer 10 bytes at a time, starting with                                                                     
- *                the byte position, then showing the hex content,                                                                      
- *                then displaying printable charcaters.                                                                                 
- * @b: The buffer to be printed.                                                                                                        
- * @size: The number of bytes to be printed from the buffer.                                                                            
- */                                                                                                                                     
-void print_buffer(char *b, int size)                                                                                                    
-{                                                                                                                                       
-        int byte, index;
-        
-        for (byte = 0; byte < size; byte += 10)                                                                                         
-        {                                                                                                                               
-                printf("%08x: ", byte);                                                                                                 
-                                                                                                                                        
-                for (index = 0; index < 10; index++)                                                                                    
-                {                                                                                                                       
-                        if ((index + byte) >= size)                                                                                     
-                                printf("  ");                                                                                           
-                                                                                                                                        
-                        else                                                                                                            
-                                printf("%02x", *(b + index + byte));                                                                    
-                                                                                                                                        
-                        if ((index % 2) != 0 && index != 0)                                                                             
-                                printf(" ");                                                                                            
-                }                                                                                                                       
-                                                                                                                                        
-                for (index = 0; index < 10; index++)                                                                                    
-                {                                                                                                                       
-                        if ((index + byte) >= size)                                                                                     
-                                break;                                                                                                  
-                                                                                                                                        
-                        else if (*(b + index + byte) >= 31 &&                                                                           
-                                 *(b + index + byte) <= 126)                                                                            
-                                printf("%c", *(b + index + byte));                                                                      
-                                                                                                                                        
-                        else                                                                                                            
-                                printf(".");                                                                                            
-                }                                                                                                                       
-                                                                                                                                        
-                if (byte >= size)                                                                                                       
-                        continue;                                                                                                       
-                                                                                                                                        
-                printf("\n");                                                                                                           
-        }                                                                                                                               
-                                                                                                                                        
-        if (size <= 0)                                                                                                                  
-                printf("\n");                                                                                                           
+i--;
+for (j = 0; j < i; j++, i--)
+{
+temp = *(n + j);
+*(n + j) = *(n + i);
+*(n + i) = temp;
+}
+}
+/**
+ * infinite_add - add 2 numbers together
+ * @n1: text representation of 1st number to add
+ * @n2: text representation of 2nd number to add
+ * @r: pointer to buffer
+ * @size_r: buffer size
+ * Return: pointer to calling function
+ */
+char *infinite_add(char *n1, char *n2, char *r, int size_r)
+{
+int overflow = 0, i = 0, j = 0, digits = 0;
+int val1 = 0, val2 = 0, temp_tot = 0;
+while (*(n1 + i) != '\0')
+i++;
+while (*(n2 + j) != '\0')
+j++;
+i--;
+j--;
+if (j >= size_r || i >= size_r)
+return (0);
+while (j >= 0 || i >= 0 || overflow == 1)
+{
+if (i < 0)
+val1 = 0;
+else
+val1 = *(n1 + i) - '0';
+if (j < 0)
+val2 = 0;
+else
+val2 = *(n2 + j) - '0';
+temp_tot = val1 + val2 + overflow;
+if (temp_tot >= 10)
+overflow = 1;
+else
+overflow = 0;
+if (digits >= (size_r - 1))
+return (0);
+*(r + digits) = (temp_tot % 10) + '0';
+digits++;
+j--;
+i--;
+}
+if (digits == size_r)
+return (0);
+*(r + digits) = '\0';
+rev_string(r);
+return (r);
 }
