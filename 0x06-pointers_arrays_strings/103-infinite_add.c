989f667e83aff9acea057ccb189386f9b1de675c
faiz <faizfarouk8@gmail.com>
2023-03-02 23:32:29 -0800

    103-infinite_add.c

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -1,9 +1,11 @@
 #include "main.h"
+
 /**
  * rev_string - reverse array
  * @n: integer params
  * Return: 0
  */
+
 void rev_string(char *n)
 {
 int i = 0;
@@ -62,10 +64,4 @@ return (0);
 digits++;
 j--;
 i--;
-}
-if (digits == size_r)
-return (0);
-*(r + digits) = '\0';
-rev_string(r);
-return (r);
 }
