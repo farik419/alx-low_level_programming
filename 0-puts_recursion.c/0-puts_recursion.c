967ad6a24edcc76921a3deb7978e0c546768937a
faiz <faizfarouk8@gmail.com>
2023-03-07 04:03:04 -0800

    update

--- 0-puts_recursion.c/0-puts_recursion.c
+++ 0-puts_recursion.c/0-puts_recursion.c
@@ -1,16 +1,21 @@
+/*
+ * File: 0-puts_recursion.c 
+ * Auth: Faiz
+ */ 
+
 #include "main.h"
+
 /**
- * _puts_recursion - function like puts();
- * @s: input
- * Return: Always 0 (Success)
+ * _puts_recursion - Prints a string, followed by a new line.
+ * @s: The string to be printed.
  */
 void _puts_recursion(char *s)
 {
 if (*s)
-{
-_putchar(*s);
-_puts_recursion(s + 1);
+{                                                                                                                               
+_putchar(*s);                                                                                                           
+_puts_recursion(s + 1);                                                                                                 
 }
-else
-_putchar('\n');
+else                                                                                                                            
+_putchar('\n');                                                                                                         
 }
